3b27b4bb389f47f9da741c72b2456608b8a557bf
praks0208 <praks.sawant26@gmail.com>
2024-06-10 20:21:26 +0530

    Add Question Timers

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function QuestionTimer({timeout, onTimeOut}){
+const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        setTimeout(onTimeOut, timeout);
+    }, [timeout, onTimeOut]);
+
+    useEffect(() => {
+        setInterval(()=>{
+            setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+        }, 100);
+    }, []);
+
+    return <progress id="question-time" max={timeout} value={remainingTime} />;
+}
