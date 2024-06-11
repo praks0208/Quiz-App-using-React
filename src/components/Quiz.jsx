dcc1d6084e6913838131f794ef444fab7a07e289
praks0208 <praks.sawant26@gmail.com>
2024-06-11 12:56:53 +0530

    Highlighting Selected Answers & Managing More State

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,18 +5,31 @@ import QuestionTimer from './QuestionTimer.jsx';
 import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
+    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    const handleSelectAnswer = useCallback (function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setAnswerState('answered');
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }, []);
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct');
+            } else {
+                setAnswerState('wrong');
+            }
 
-    const handleSkipAnswer = useCallback(()=> handleSelectAnswer(null), [handleSelectAnswer]);
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000)
+        }, 1000)
+    }, [activeQuestionIndex]);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
         return (
@@ -33,20 +46,33 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
-            <QuestionTimer 
-            key={activeQuestionIndex}
-            timeout={10000} 
-            onTimeOut={handleSkipAnswer} />
-            
+                <QuestionTimer
+                    key={activeQuestionIndex}
+                    timeout={10000}
+                    onTimeOut={handleSkipAnswer} />
+
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>
+                    {shuffledAnswers.map((answer) => {
+                        const isSelected = userAnswers[userAnswers.length - 1] === answer;
+                        let cssClass = '';
+
+                        if (answerState === 'answered' && isSelected) {
+                            cssClass = 'selected';
+                        }
+
+                        if(answerState === 'correct' || answerState === 'wrong' && isSelected){
+                            cssClass = answerState;
+                        }
+
+                        return <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
                                 {answer}
                             </button>
                         </li>
-                    ))}
+                    }
+
+                    )}
                 </ul>
             </div>
         </div>
